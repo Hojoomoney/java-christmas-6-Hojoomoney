0bcb101cb5d907f8216ad1beee6690c53eea853b
Hojoomoney <kimhojoo51@gmail.com>
2023-11-09 23:11:16 +0900

    Feat : 식당 예상 방문 날짜 입력 기능

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -3,5 +3,19 @@ package christmas;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	OutputView outputView = new OutputView();
+    	InputView inputView = new InputView();
+    	outputView.printIntro();
+    	while(true) {
+    		try {
+    			inputView.readDate();
+    			break;
+    		} catch(NumberFormatException e) {
+    			System.out.println("[ERROR] 숫자를 입력해 주세요.");
+    		} catch(IllegalArgumentException e) {
+    			System.out.println(e.getMessage());
+    		} 
+    	}
+    	
     }
 }
