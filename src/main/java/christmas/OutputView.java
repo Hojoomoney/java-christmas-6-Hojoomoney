80b9472d87a08daebe1f6b57da4ce3827578b72e
Hojoomoney <kimhojoo51@gmail.com>
2023-11-10 01:13:54 +0900

    Feat : 주문한 메뉴 출력 기능

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,12 +1,21 @@
 package christmas;
 
+import java.util.*;
+
 public class OutputView {
 	
 	public void printIntro() {
 		System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 	}
-	public void printMenu() {
-		System.out.println("<주문 메뉴>");
+	public void printDate(int date) {
+		System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
 	}
-
+	public void printMenu(Map<String, Integer> menu) {
+		System.out.println();
+		System.out.println("<주문 메뉴>");
+		for (Map.Entry<String, Integer> entry : menu.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue() + "개");
+		}
+	}
+	
 }
