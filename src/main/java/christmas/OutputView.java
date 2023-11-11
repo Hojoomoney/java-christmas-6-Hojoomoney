c8cb55629e90c858a18c51356141eb6fd5e8f722
Hojoomoney <kimhojoo51@gmail.com>
2023-11-11 17:09:57 +0900

    Feat : 할인 전 총주문 금액 출력 기능

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,21 +1,35 @@
 package christmas;
 
-import java.util.*;
+import java.text.DecimalFormat;
+import java.util.Map;
 
 public class OutputView {
-	
+	DecimalFormat df = new DecimalFormat("###,###원");
+	//인트로 출력 기능
 	public void printIntro() {
 		System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 	}
+	//날짜 출력 기능
 	public void printDate(int date) {
 		System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
 	}
-	public void printMenu(Map<String, Integer> menu) {
+	//주문한 메뉴 출력 기능
+	public void printMenu(Map<Menu, Integer> menu) {
 		System.out.println();
 		System.out.println("<주문 메뉴>");
-		for (Map.Entry<String, Integer> entry : menu.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue() + "개");
+		for (Map.Entry<Menu, Integer> entry : menu.entrySet()) {
+			System.out.println(entry.getKey().getKoreanName() + " " + entry.getValue() + "개");
 		}
 	}
-	
+	//할인 전 총주문 금액 출력
+	public int printTotalPriceBeforeDiscount(Map<Menu, Integer> menu) {
+		int total = 0;
+		for (Map.Entry<Menu, Integer> entry : menu.entrySet()) {
+			total += entry.getKey().getPrice() * entry.getValue();
+		}
+		System.out.println();
+		System.out.println("<할인 전 총주문 금액>");
+		System.out.println(df.format(total));
+		return total;
+	}
 }
