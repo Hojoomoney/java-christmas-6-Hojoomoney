bc99a22d3c8927ffcb2f6630fcb14105249bf6a3
Hojoomoney <kimhojoo51@gmail.com>
2023-11-11 17:27:29 +0900

    Feat : 크리스마스 디데이 할인 확인 메소드 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,21 +1,33 @@
 package christmas;
 
+import java.util.*;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.EnumSelector;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     	OutputView outputView = new OutputView();
-    	InputView inputView = new InputView();
+    	CheckValidate checkValid = new CheckValidate();
     	outputView.printIntro();
-    	while(true) {
-    		try {
-    			inputView.readDate();
-    			break;
-    		} catch(NumberFormatException e) {
-    			System.out.println("[ERROR] 숫자를 입력해 주세요.");
-    		} catch(IllegalArgumentException e) {
-    			System.out.println(e.getMessage());
-    		} 
-    	}
+    	int date = checkValid.checkValidDate();
+    	Map<Menu, Integer> validMenu = new LinkedHashMap<>();
+    	validMenu = checkValid.checkValidMenu();
+    	outputView.printDate(date);
+    	outputView.printMenu(validMenu);
+    	int total = outputView.printTotalPriceBeforeDiscount(validMenu);
+    	boolean giveaway = outputView.printGiveaway(total);
+    	int christmasDaySaleAmount = checkChristmasDaySale(date);
     	
+    }
+    
+    //크리스마스 디데이 할인 확인 메소드
+    private static int checkChristmasDaySale(int day) {
+    	int saleAmount = 0;
+    	if(day < 26) {
+    		saleAmount = 1000 + (day-1)*100;
+    		return saleAmount;
+    	}
+    	return saleAmount;
     }
 }
