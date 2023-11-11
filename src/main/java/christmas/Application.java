a1eeec57ce01682f3ffd26193fdbcddfb3042c15
Hojoomoney <kimhojoo51@gmail.com>
2023-11-11 18:09:10 +0900

    Feat : 평일 할인 및 주말 할인 확인,계산 메소드 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -17,17 +17,19 @@ public class Application {
     	int total = outputView.printTotalPriceBeforeDiscount(validMenu);
     	boolean giveaway = outputView.printGiveaway(total);
     	int christmasDaySaleAmount = checkChristmasDaySale(day);
+    	int dayOfWeekNumber = findDayOfWeek(day);
+    	int dayOfWeekSaleAmount = checkDayOfWeekSale(dayOfWeekNumber, validMenu);
     	
     }
     
     //크리스마스 디데이 할인 확인 메소드
     private static int checkChristmasDaySale(int day) {
-    	int saleAmount = 0;
+    	int christmasDaySaleAmount = 0;
     	if(day < 26) {
-    		saleAmount = 1000 + (day-1)*100;
-    		return saleAmount;
+    		christmasDaySaleAmount -= 1000 + (day-1)*100;
+    		return christmasDaySaleAmount;
     	}
-    	return saleAmount;
+    	return christmasDaySaleAmount;
     }
     //특정 날짜의 요일 구하기
     private static int findDayOfWeek(int day) {
@@ -35,5 +37,38 @@ public class Application {
     	DayOfWeek dayOfWeek = date.getDayOfWeek();
     	int dayOfWeekNumber = dayOfWeek.getValue();
     	return dayOfWeekNumber;
+    }
+    //평일 주말 확인 메소드
+    private static int checkDayOfWeekSale(int dayOfWeekNumber, Map<Menu, Integer> validMenu) {
+    	int dayOfWeekSaleAmount = 0;
+    	//주말일때
+    	if(dayOfWeekNumber == 5 || dayOfWeekNumber == 6) {
+    		dayOfWeekSaleAmount = calculateWeekendSale(validMenu);
+    	}
+    	//평일일때
+    	if (!(dayOfWeekNumber == 5 || dayOfWeekNumber == 6)) {
+    		dayOfWeekSaleAmount = calculateWeekdaySale(validMenu);
+		}
+    	return dayOfWeekSaleAmount;
+    }
+    //평일 할인 계산 메소드
+    private static int calculateWeekdaySale(Map<Menu, Integer> validMenu) {
+    	int dayOfWeekSaleAmount = 0;
+    	for (Map.Entry<Menu, Integer> entry : validMenu.entrySet()) {
+			if(entry.getKey().getTypeMenu().equals("dessert")) {
+				dayOfWeekSaleAmount -= entry.getValue() * 2023; 
+			}
+		}
+    	return dayOfWeekSaleAmount;
+    }
+    //주말 할인 계산 메소드
+    private static int calculateWeekendSale(Map<Menu, Integer> validMenu) {
+    	int dayOfWeekSaleAmount = 0;
+    	for (Map.Entry<Menu, Integer> entry : validMenu.entrySet()) {
+    		if(entry.getKey().getTypeMenu().equals("main")) {
+				dayOfWeekSaleAmount -= entry.getValue() * 2023; 
+			}
+		}
+    	return dayOfWeekSaleAmount;
     }
 }
