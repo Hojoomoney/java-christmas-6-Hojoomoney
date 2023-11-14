5609420ede4e2cb7dac3595a127764d633ddf449
Hojoomoney <kimhojoo51@gmail.com>
2023-11-14 22:12:03 +0900

    Fix : 메뉴의 형식이 다른 경우 오류 발생 수정

--- src/main/java/christmas/CheckValidate.java
+++ src/main/java/christmas/CheckValidate.java
@@ -33,6 +33,8 @@ public class CheckValidate {
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 				validMenu.clear();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println(ERROR_MSG);
 			}
     		validOrder = validMenu.size() == menu.length;
     	}
